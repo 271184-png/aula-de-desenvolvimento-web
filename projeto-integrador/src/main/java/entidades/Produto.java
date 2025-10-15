75e26b171943e6a22f8cbf9b50fcda4530429e7d
Jean Lucas L <271184@unifio.edu.br>
2025-10-15 23:34:12 +0000

    15-10

--- projeto-integrador/src/main/java/entidades/Produto.java
+++ projeto-integrador/src/main/java/entidades/Produto.java
@@ -2,8 +2,7 @@ package entidades;
 
 import java.time.LocalDate;
 
-
-
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +11,7 @@ import jakarta.persistence.ManyToOne;
 
 
 @Entity
-public class Produto {
+public class Produto extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
